24a132298eda336511bf95d15cf765a3062334b3
Ren <116741314+123R3N321@users.noreply.github.com>
2022-11-20 18:47:44 -0500

    Add files via upload

---
+++ Jiyuan_Ren.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Hello
+{
+	public Hello()
+	{
+        static void Main(string[] args) global::System.Object value = Console.WriteLine("Hello World! Ren here. Lemme survive midterm and Imma come back here");
+
+    }
+}
